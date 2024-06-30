41046ef2006d92a1c1c5d2498113466f80cd38b3
Kyaw Naing Thu <kyawnaingthu225@gmail.com>
2024-06-30 21:59:08 +0700

    add logging

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/policy', (req, res) => {
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
+    console.log('webhook get calling');
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = "MY_LOCAL_TESTING";
     
@@ -42,6 +43,8 @@ app.get('/webhook', (req, res) => {
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);      
+            console.log('error');
+
         }
     }
 });
@@ -49,9 +52,11 @@ app.get('/webhook', (req, res) => {
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
     let body = req.body;
-
+    console.log('webhook post');
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
+        console.log('worked');
+
         body.entry.forEach(function(entry) {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
@@ -76,6 +81,7 @@ app.post('/webhook', (req, res) => {
     } else {
         // Returns a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
+        console.log('404');
     }
 });
 
